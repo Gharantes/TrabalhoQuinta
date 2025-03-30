5517b40bef01613be113af9ceeca37d6330a0ddb
Guilherme Arantes <work.gharantes@gmail.com>
2025-03-29 22:47:56 -0300

    Atv8 - Contador Incremental

--- atv8/src/App.tsx
+++ atv8/src/App.tsx
@@ -1,24 +1,56 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [isTimerActive, setIsTimerActive] = useState(true);
+  const timerRef = useRef<number | undefined>(undefined);
+  const [timer, setTimer] = useState(0);
+
+  const divRef = useRef<HTMLButtonElement>(null); // Create a ref for the name input
+
+  useEffect(() => {
+    setButtonText();
+
+    if (isTimerActive) {
+      clear()
+      timerRef.current = setInterval(() => {
+        incrementTimer();
+      }, 1000);
+    } else {
+      clearInterval(timerRef.current);
+      timerRef.current = undefined;
+    }
+  }, [isTimerActive]);
+
+  function clear() {
+    if (timerRef.current) {
+      clearInterval(timerRef.current);
+    }
+  }
+  function incrementTimer() {
+    setTimer((prev) => {
+      return prev+1;
+    })
+  }
+  function setButtonText() {
+    if (divRef.current) {
+      if (isTimerActive) {
+        divRef.current.textContent = "Pausar"
+      } else {
+        divRef.current.textContent = "Retomar"
+      }
+    }
+  }
+  function toggleTimer() {
+    setIsTimerActive(!isTimerActive);
+  }
+
 
   return (
     <>
-      <div className="card">
-        <div className='display'>{count}</div>
-        
-        <div className='btn-line'>
-          <button onClick={() => setCount((count) => count - 1)}>
-            Decrementar
-          </button>
-
-          <button onClick={() => setCount((count) => count + 1)}>
-            Incrementar
-          </button>
-        </div>
-      </div>
+      <div>{ timer }</div>
+      <button ref={divRef} onClick={toggleTimer}></button>
     </>
   )
 }
