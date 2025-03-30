f8b822346b73a64c6d95926225c6682609572dc5
Guilherme Arantes <work.gharantes@gmail.com>
2025-03-29 21:13:30 -0300

    Atv4 - Api Call

--- atv4/src/App.tsx
+++ atv4/src/App.tsx
@@ -1,24 +1,27 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { OuterApiResponse } from './types/ApiResponse'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [apiData, setApiData] = useState<OuterApiResponse>({amiibo: []})
+  
+  function getApiData(){
+    axios.get('https://www.amiiboapi.com/api/amiibo/').then((response) => {
+      setApiData(response.data)
+    }).catch((error) => console.log(error))
+  }
+
+  useEffect(() => {
+    getApiData();
+  }, [])
+
 
   return (
     <>
-      <div className="card">
-        <div className='display'>{count}</div>
-        
-        <div className='btn-line'>
-          <button onClick={() => setCount((count) => count - 1)}>
-            Decrementar
-          </button>
-
-          <button onClick={() => setCount((count) => count + 1)}>
-            Incrementar
-          </button>
-        </div>
-      </div>
+      { apiData.amiibo.map((v, index) => (
+        <div key={index}>{v.name} - {v.gameSeries}</div>
+      ))}
     </>
   )
 }
